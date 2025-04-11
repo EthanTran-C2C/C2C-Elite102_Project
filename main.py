fa02eaaa0b3241452bd32ecf90dd16e4b68f66bc
EthanTran-C2C <ethanrtran.us@gmail.com>
2025-04-11 06:50:19 +0000

    Created interface

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Hello, and welcome to the Banking Application")
+print("Here is a list of options to choose from ")
+print("1: Cash a check")
+print("2: Deposit or withdraw funds")
+print("3: Register or delete an account")
+print("4: Modify account information")
+user_input = input("Please select an option from the menu:")
