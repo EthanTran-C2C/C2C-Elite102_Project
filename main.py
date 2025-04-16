43555fe51bafb555597c55f8947a8620b9e989c2
Ethan <ethanrtran.us@gmail.com>
2025-04-15 22:51:57 -0500

    class def

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ import unittest
 from tkinter import *
 from tkinter import ttk
 
-print("Hello, and welcome to the Banking Application")
-print("Here is a list of options to choose from ")
-print("1: Cash a check")
-print("2: Deposit or withdraw funds")
-print("3: Register or delete an account")
-print("4: Modify account information")
-user_input = input("Please select an option from the menu:")
+class bankingapplilcation:
+    def ui():
+        print("Hello, and welcome to the Banking Application")
+        print("Here is a list of options to choose from ")
+        print("1: Cash a check")
+        print("2: Deposit or withdraw funds")
+        print("3: Register or delete an account")
+        print("4: Modify account information")
+        user_input = input("Please select an option from the menu:")
+
+    def check_balance():
+
+    def deposit():
+
+    def withdraw():
+    
+    def create_account():
+    
+    def close_account():
+
+    def modify_account():
+        
